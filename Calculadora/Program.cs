3ec20680a805b3fbd05e3a973e15b0ca2e984bba
Bernardo Claure <Bernardo.Claure@niceincontact.com>
2022-09-01 11:13:15 -0400

    rebased

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int multiplicacion (int num1,int num2)
-{
-    return num1 * num2; 
-}
 
 float division(float num1,float num2)
 {
     return num1 / num2; 
+}
+
+int multiplicacion (int num1,int num2)
+{
+    return num1 * num2; 
 }
