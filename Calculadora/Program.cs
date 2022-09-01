89d5586c33a32692b143ab196da154c248c246f7
Bernardo Claure <Bernardo.Claure@niceincontact.com>
2022-09-01 10:11:23 -0400

    ft funcion division

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine(multiplicacion(2,3));
 
 int multiplicacion (int num1,int num2)
 {
     return num1 * num2; 
+}
+
+float division(float num1,float num2)
+{
+    return num1 / num2; 
 }
